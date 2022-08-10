a792d2c6aef677adb3753573b45cfa67fec80467
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-08-10 21:09:27 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,20 @@ for (let anchor of anchors) {
   })
 }
 
+function setColor(e){
+     
+  if(e.type==="mouseover" || e.type==="onclick")
+    e.target.style.color = "white";
+  else if(e.type==="mouseout")
+    e.target.style.color = "#1d1d1d";
+}
+
+var blueRects = document.querySelectorAll(".block");
+var a = document.querySelectorAll(".a");
+for (let index = 0; index < blueRects.length; index++) {
+  const react = a[index];
+  react.addEventListener("mouseover", setColor);
+  react.addEventListener("onclick", setColor);
+  react.addEventListener("mouseout", setColor);
+}
+
